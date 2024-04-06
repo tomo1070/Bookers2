da02c4b523e8b01e6b795d6a9ed4d422bb2d744c
tomo1070 <160234199+tomo1070@users.noreply.github.com>
2024-04-06 14:40:16 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to Book_path
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :image, :body)
+  end
+end
